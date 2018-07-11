33d2ab325aa61869c352c62e4e0f515ba916f4a4
Edward <edward.gray@yandex.ru>
2018-07-11 17:29:56 +0300

    Add Equipment form

--- router/web/refs/equipment.js
+++ router/web/refs/equipment.js
@@ -29,7 +29,40 @@ module.exports = function () {
           }
         });
     });
-    // res.render('refs/equipment.ejs');
   });
+  
+  router.get('/edit/:id', function (req, res) {
+    var id = req.params.id;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+        ' FROM equipments a' + 
+        ' WHERE a.equipment_id = ?' +
+        ' ORDER BY a.name ASC', [id], function (err, rows) {
+          if (err) {
+            throw err;
+          }
+          connection.release();
+
+          if (err) {
+            console.error(err);
+            res.status(500).send({
+              code: 500,
+              msg: 'Database error'
+            });
+          } else {
+            res.render('refs/forms/equipment.ejs', { data: rows[0] });
+          }
+        });
+      });
+  });
+
+  router.post('/save', function (req, res) {
+    // console.log('req.body', req.body);
+    // console.log('req.body.name', req.body.name);
+    // console.log('req.body.years', req.body.years);
+    res.redirect('/equipment');
+  });
+
   return router;
 };
