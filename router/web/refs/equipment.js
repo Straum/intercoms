f2b9348763108ae62562cac9236f30949b7c7c15
Edward <edward.gray@yandex.ru>
2018-07-17 17:14:25 +0300

    Implemented Equipment delete

--- router/web/refs/equipment.js
+++ router/web/refs/equipment.js
@@ -11,7 +11,8 @@ module.exports = function () {
     var pageCount = 0;
     db.get().getConnection(function (err, connection) {
       connection.query(
-        ' SELECT COUNT(*) AS count FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
+        ' SELECT COUNT(*) AS count' + 
+        ' FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
           connection.release();
           pageCount = Math.ceil(rows[0].count / visibleRows);
         }
@@ -37,49 +38,6 @@ module.exports = function () {
               });
             } else {
               var currentPage = 1;
-              res.render('refs/equipment.ejs', { 
-                'data': rows, 
-                'pageCount': pageCount, 
-                'currentPage': currentPage
-              });
-            }
-          });
-      });
-    });
-  });
-
-  router.get('/:offset', function (req, res) {
-    var offset = +req.params.offset;
-    var pageCount = 0;
-    db.get().getConnection(function (err, connection) {
-      connection.query(
-        ' SELECT COUNT(*) AS count FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
-          connection.release();
-          pageCount = Math.ceil(rows[0].count / visibleRows);
-        }
-      );
-
-      db.get().getConnection(function (err, connection) {
-        connection.query(
-          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
-          ' FROM equipments a' + 
-          ' WHERE a.equipment_id > 0' +
-          ' ORDER BY a.name ASC' +
-          ' LIMIT ?' + 
-          ' OFFSET ?', [visibleRows, offset], function (err, rows) {
-            if (err) {
-              throw err;
-            }
-            connection.release();
-
-            if (err) {
-              console.error(err);
-              res.status(500).send({
-                code: 500,
-                msg: 'Database error'
-              });
-            } else {
-              var currentPage = Math.ceil(offset / visibleRows) + 1;
               res.render('refs/equipment.ejs', { 
                 'data': rows, 
                 'pageCount': pageCount, 
@@ -122,6 +80,53 @@ module.exports = function () {
     res.render('refs/forms/equipment.ejs');
   });
 
+  router.get('/:offset', function (req, res) {
+    var offset = +req.params.offset;
+    var pageCount = 0;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT COUNT(*) AS count' +
+        ' FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
+          connection.release();
+          pageCount = Math.ceil(rows[0].count / visibleRows);
+          if ((offset > pageCount * visibleRows)) {
+            offset = (pageCount - 1) * visibleRows;
+          }
+        }
+      );
+
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+          ' FROM equipments a' + 
+          ' WHERE a.equipment_id > 0' +
+          ' ORDER BY a.name ASC' +
+          ' LIMIT ?' + 
+          ' OFFSET ?', [visibleRows, offset], function (err, rows) {
+            if (err) {
+              throw err;
+            }
+            connection.release();
+
+            if (err) {
+              console.error(err);
+              res.status(500).send({
+                code: 500,
+                msg: 'Database error'
+              });
+            } else {
+              var currentPage = Math.ceil(offset / visibleRows) + 1;
+              res.render('refs/equipment.ejs', { 
+                'data': rows, 
+                'pageCount': pageCount, 
+                'currentPage': currentPage
+              });
+            }
+          });
+      });
+    });
+  });
+
   router.post('/save', function (req, res) {
     if ((req.body.id) && (isFinite(+req.body.id))) {
       db.get().getConnection(function (err, connection) {
@@ -142,16 +147,36 @@ module.exports = function () {
       db.get().getConnection(function (err, connection) {
         connection.query(
           ' INSERT INTO equipments (name, guarantee_period)' + 
-          ' VALUE(?, ?)', [req.body.name, req.body.years], function(err) {
+          ' VALUE(?, ?)', [req.body.name, req.body.years], function (err) {
             connection.release();            
             if (err) {
               res.status(500).send({code: 500, msg: 'Database Error'});
-            }else{
+            }else {
               res.redirect('/equipment');
             }
           }
         );
       });
+    }
+  });
+
+  router.post('/delete', function (req, res) {
+    if ((req.body.id) && (isFinite(+req.body.id))) {
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' DELETE FROM equipments WHERE equipment_id = ?', [+req.body.id], function (err) {
+            connection.release();           
+            if (err) {
+              res.status(500).send({code: 500, msg: 'Database Error'});
+            } else {
+              res.status(200).send({'result': 'OK'});
+            }
+          }
+        );
+      });
+    }
+    else {
+      res.status(500).send({code: 500, msg: 'Incorrect parameter'});
     }
   });
 
