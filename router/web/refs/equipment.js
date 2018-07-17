a44a47c54ff50ea4a2ddb0598926dd2ba356638f
Edward <edward.gray@yandex.ru>
2018-07-17 11:56:36 +0300

    Implemented Paging Equipment

--- router/web/refs/equipment.js
+++ router/web/refs/equipment.js
@@ -2,32 +2,92 @@
 
 const express = require('express');
 var db = require('../../../lib/db.js');
+const visibleRows = 20;
 
 module.exports = function () {
   var router = express.Router();
 
   router.get('/', function (req, res) {
+    var pageCount = 0;
     db.get().getConnection(function (err, connection) {
       connection.query(
-        ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
-        ' FROM equipments a' + 
-        ' WHERE a.equipment_id > 0' +
-        ' ORDER BY a.name ASC', [], function (err, rows) {
-          if (err) {
-            throw err;
-          }
+        ' SELECT COUNT(*) AS count FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
           connection.release();
+          pageCount = Math.ceil(rows[0].count / visibleRows);
+        }
+      );
 
-          if (err) {
-            console.error(err);
-            res.status(500).send({
-              code: 500,
-              msg: 'Database error'
-            });
-          } else {
-            res.render('refs/equipment.ejs', { data: rows });
-          }
-        });
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+          ' FROM equipments a' + 
+          ' WHERE a.equipment_id > 0' +
+          ' ORDER BY a.name ASC' +
+          ' LIMIT ?', [visibleRows], function (err, rows) {
+            if (err) {
+              throw err;
+            }
+            connection.release();
+
+            if (err) {
+              console.error(err);
+              res.status(500).send({
+                code: 500,
+                msg: 'Database error'
+              });
+            } else {
+              var currentPage = 1;
+              res.render('refs/equipment.ejs', { 
+                'data': rows, 
+                'pageCount': pageCount, 
+                'currentPage': currentPage
+              });
+            }
+          });
+      });
+    });
+  });
+
+  router.get('/:offset', function (req, res) {
+    var offset = +req.params.offset;
+    var pageCount = 0;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT COUNT(*) AS count FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
+          connection.release();
+          pageCount = Math.ceil(rows[0].count / visibleRows);
+        }
+      );
+
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+          ' FROM equipments a' + 
+          ' WHERE a.equipment_id > 0' +
+          ' ORDER BY a.name ASC' +
+          ' LIMIT ?' + 
+          ' OFFSET ?', [visibleRows, offset], function (err, rows) {
+            if (err) {
+              throw err;
+            }
+            connection.release();
+
+            if (err) {
+              console.error(err);
+              res.status(500).send({
+                code: 500,
+                msg: 'Database error'
+              });
+            } else {
+              var currentPage = Math.ceil(offset / visibleRows) + 1;
+              res.render('refs/equipment.ejs', { 
+                'data': rows, 
+                'pageCount': pageCount, 
+                'currentPage': currentPage
+              });
+            }
+          });
+      });
     });
   });
   
