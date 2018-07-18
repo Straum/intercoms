95ce0f9d4b3939ec91f0ae4ad6b6e33c1606bfad
Edward <edward.gray@yandex.ru>
2018-07-18 17:41:07 +0300

    Clients reference is done

--- router/web/refs/clients.js
+++ router/web/refs/clients.js
@@ -1,12 +1,188 @@
 'use strict';
 
 const express = require('express');
+var db = require('../../../lib/db.js');
+const visibleRows = 20;
 
 module.exports = function () {
   var router = express.Router();
 
   router.get('/', function (req, res) {
-    res.render('refs/clients.ejs');
+    var pageCount = 0;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT COUNT(*) AS count' +
+        ' FROM clients WHERE client_id > 0', [], function (err, rows) {
+          connection.release();
+          pageCount = Math.ceil(rows[0].count / visibleRows);
+
+          db.get().getConnection(function (err, connection) {
+            connection.query(
+              ' SELECT a.client_id AS id, a.name' +
+              ' FROM clients a' +
+              ' WHERE a.client_id > 0' +
+              ' ORDER BY a.name ASC' +
+              ' LIMIT ?', [visibleRows], function (err, rows) {
+                if (err) {
+                  throw err;
+                }
+                connection.release();
+
+                if (err) {
+                  console.error(err);
+                  res.status(500).send({
+                    'code': 500,
+                    'msg': 'Database error'
+                  });
+                } else {
+                  var currentPage = 1;
+                  res.render('refs/clients.ejs', {
+                    'data': rows,
+                    'pageCount': pageCount,
+                    'currentPage': currentPage,
+                    'visibleRows': visibleRows
+                  });
+                }
+              });
+          });
+        });
+    });
   });
+
+  router.get('/edit/:id', function (req, res) {
+    var id = req.params.id;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT a.client_id AS id, a.name' +
+        ' FROM clients a' +
+        ' WHERE a.client_id = ?', [id], function (err, rows) {
+          if (err) {
+            throw err;
+          }
+          connection.release();
+
+          if (err) {
+            console.error(err);
+            res.status(500).send({
+              'code': 500,
+              'msg': 'Database error'
+            });
+          } else {
+            res.render('refs/forms/clients.ejs', {
+              data: rows[0]
+            });
+          }
+        });
+    });
+  });
+
+  router.get('/add', function (req, res) {
+    res.render('refs/forms/clients.ejs');
+  });
+
+  router.get('/:offset', function (req, res) {
+    var offset = +req.params.offset;
+    var pageCount = 0;
+    db.get().getConnection(function (err, connection) {
+      connection.query(
+        ' SELECT COUNT(*) AS count' +
+        ' FROM clients WHERE client_id > 0', [], function (err, rows) {
+          connection.release();
+          pageCount = Math.ceil(rows[0].count / visibleRows);
+          if ((offset > pageCount * visibleRows)) {
+            offset = (pageCount - 1) * visibleRows;
+          }
+
+          db.get().getConnection(function (err, connection) {
+            connection.query(
+              ' SELECT a.client_id AS id, a.name' +
+              ' FROM clients a' +
+              ' WHERE a.client_id > 0' +
+              ' ORDER BY a.name ASC' +
+              ' LIMIT ?' +
+              ' OFFSET ?', [visibleRows, offset], function (err, rows) {
+                if (err) {
+                  throw err;
+                }
+                connection.release();
+
+                if (err) {
+                  console.error(err);
+                  res.status(500).send({
+                    'code': 500,
+                    'msg': 'Database error'
+                  });
+                } else {
+                  var currentPage = Math.ceil(offset / visibleRows) + 1;
+                  res.render('refs/clients.ejs', {
+                    'data': rows,
+                    'pageCount': pageCount,
+                    'currentPage': currentPage,
+                    'visibleRows': visibleRows
+                  });
+                }
+              });
+          });
+        });
+    });
+  });
+
+  router.post('/save', function (req, res) {
+    if ((req.body.id) && (isFinite(+req.body.id))) {
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' UPDATE clients SET name = ?' +
+          ' WHERE client_id = ?', [req.body.name, req.body.id], function (err) {
+            connection.release();
+            if (err) {
+              res.status(500).send({ 'code': 500, 'msg': 'Database Error' });
+            } else {
+              res.redirect('/clients');
+            }
+          }
+        );
+      });
+    }
+    else {
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' INSERT INTO clients (name)' +
+          ' VALUE(?)', [req.body.name], function (err) {
+            connection.release();
+            if (err) {
+              res.status(500).send({ 'code': 500, 'msg': 'Database Error' });
+            } else {
+              res.redirect('/clients');
+            }
+          }
+        );
+      });
+    }
+  });
+
+  router.post('/delete', function (req, res) {
+    if ((req.body.id) && (isFinite(+req.body.id))) {
+      db.get().getConnection(function (err, connection) {
+        connection.query(
+          ' DELETE FROM clients WHERE client_id = ?', [+req.body.id], function (err) {
+            connection.release();
+            if (err) {
+              res.status(500).send({ 
+                'code': 500, 
+                'msg': 'Database Error', 
+                'err': JSON.stringify(err)
+              });
+            } else {
+              res.status(200).send({ 'result': 'OK' });
+            }
+          }
+        );
+      });
+    }
+    else {
+      res.status(500).send({ 'code': 500, 'msg': 'Incorrect parameter' });
+    }
+  });
+
   return router;
 };
--- router/web/refs/equipment.js
+++ router/web/refs/equipment.js
@@ -11,50 +11,50 @@ module.exports = function () {
     var pageCount = 0;
     db.get().getConnection(function (err, connection) {
       connection.query(
-        ' SELECT COUNT(*) AS count' + 
+        ' SELECT COUNT(*) AS count' +
         ' FROM equipments WHERE equipment_id > 0', [], function (err, rows) {
           connection.release();
           pageCount = Math.ceil(rows[0].count / visibleRows);
-        }
-      );
 
-      db.get().getConnection(function (err, connection) {
-        connection.query(
-          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
-          ' FROM equipments a' + 
-          ' WHERE a.equipment_id > 0' +
-          ' ORDER BY a.name ASC' +
-          ' LIMIT ?', [visibleRows], function (err, rows) {
-            if (err) {
-              throw err;
-            }
-            connection.release();
+          db.get().getConnection(function (err, connection) {
+            connection.query(
+              ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+              ' FROM equipments a' +
+              ' WHERE a.equipment_id > 0' +
+              ' ORDER BY a.name ASC' +
+              ' LIMIT ?', [visibleRows], function (err, rows) {
+                if (err) {
+                  throw err;
+                }
+                connection.release();
 
-            if (err) {
-              console.error(err);
-              res.status(500).send({
-                code: 500,
-                msg: 'Database error'
+                if (err) {
+                  console.error(err);
+                  res.status(500).send({
+                    'code': 500,
+                    'msg': 'Database error'
+                  });
+                } else {
+                  var currentPage = 1;
+                  res.render('refs/equipment.ejs', {
+                    'data': rows,
+                    'pageCount': pageCount,
+                    'currentPage': currentPage,
+                    'visibleRows': visibleRows
+                  });
+                }
               });
-            } else {
-              var currentPage = 1;
-              res.render('refs/equipment.ejs', { 
-                'data': rows, 
-                'pageCount': pageCount, 
-                'currentPage': currentPage
-              });
-            }
           });
-      });
+        });
     });
   });
-  
+
   router.get('/edit/:id', function (req, res) {
     var id = req.params.id;
     db.get().getConnection(function (err, connection) {
       connection.query(
         ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
-        ' FROM equipments a' + 
+        ' FROM equipments a' +
         ' WHERE a.equipment_id = ?', [id], function (err, rows) {
           if (err) {
             throw err;
@@ -64,16 +64,16 @@ module.exports = function () {
           if (err) {
             console.error(err);
             res.status(500).send({
-              code: 500,
-              msg: 'Database error'
+              'code': 500,
+              'msg': 'Database error'
             });
           } else {
-            res.render('refs/forms/equipment.ejs', { 
-              data:rows[0]
+            res.render('refs/forms/equipment.ejs', {
+              data: rows[0]
             });
           }
         });
-      });
+    });
   });
 
   router.get('/add', function (req, res) {
@@ -92,38 +92,38 @@ module.exports = function () {
           if ((offset > pageCount * visibleRows)) {
             offset = (pageCount - 1) * visibleRows;
           }
-        }
-      );
 
-      db.get().getConnection(function (err, connection) {
-        connection.query(
-          ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
-          ' FROM equipments a' + 
-          ' WHERE a.equipment_id > 0' +
-          ' ORDER BY a.name ASC' +
-          ' LIMIT ?' + 
-          ' OFFSET ?', [visibleRows, offset], function (err, rows) {
-            if (err) {
-              throw err;
-            }
-            connection.release();
+          db.get().getConnection(function (err, connection) {
+            connection.query(
+              ' SELECT a.equipment_id AS id, a.name, a.guarantee_period' +
+              ' FROM equipments a' +
+              ' WHERE a.equipment_id > 0' +
+              ' ORDER BY a.name ASC' +
+              ' LIMIT ?' +
+              ' OFFSET ?', [visibleRows, offset], function (err, rows) {
+                if (err) {
+                  throw err;
+                }
+                connection.release();
 
-            if (err) {
-              console.error(err);
-              res.status(500).send({
-                code: 500,
-                msg: 'Database error'
+                if (err) {
+                  console.error(err);
+                  res.status(500).send({
+                    'code': 500,
+                    'msg': 'Database error'
+                  });
+                } else {
+                  var currentPage = Math.ceil(offset / visibleRows) + 1;
+                  res.render('refs/equipment.ejs', {
+                    'data': rows,
+                    'pageCount': pageCount,
+                    'currentPage': currentPage,
+                    'visibleRows': visibleRows
+                  });
+                }
               });
-            } else {
-              var currentPage = Math.ceil(offset / visibleRows) + 1;
-              res.render('refs/equipment.ejs', { 
-                'data': rows, 
-                'pageCount': pageCount, 
-                'currentPage': currentPage
-              });
-            }
           });
-      });
+        });
     });
   });
 
@@ -131,12 +131,12 @@ module.exports = function () {
     if ((req.body.id) && (isFinite(+req.body.id))) {
       db.get().getConnection(function (err, connection) {
         connection.query(
-          ' UPDATE equipments SET name = ?, guarantee_period = ?' + 
-          ' WHERE equipment_id = ?', [req.body.name, req.body.years, req.body.id], function(err) {
-            connection.release();            
+          ' UPDATE equipments SET name = ?, guarantee_period = ?' +
+          ' WHERE equipment_id = ?', [req.body.name, req.body.years, req.body.id], function (err) {
+            connection.release();
             if (err) {
-              res.status(500).send({code: 500, msg: 'Database Error'});
-            }else{
+              res.status(500).send({ 'code': 500, 'msg': 'Database Error' });
+            } else {
               res.redirect('/equipment');
             }
           }
@@ -146,12 +146,12 @@ module.exports = function () {
     else {
       db.get().getConnection(function (err, connection) {
         connection.query(
-          ' INSERT INTO equipments (name, guarantee_period)' + 
+          ' INSERT INTO equipments (name, guarantee_period)' +
           ' VALUE(?, ?)', [req.body.name, req.body.years], function (err) {
-            connection.release();            
+            connection.release();
             if (err) {
-              res.status(500).send({code: 500, msg: 'Database Error'});
-            }else {
+              res.status(500).send({ 'code': 500, 'msg': 'Database Error' });
+            } else {
               res.redirect('/equipment');
             }
           }
@@ -165,18 +165,22 @@ module.exports = function () {
       db.get().getConnection(function (err, connection) {
         connection.query(
           ' DELETE FROM equipments WHERE equipment_id = ?', [+req.body.id], function (err) {
-            connection.release();           
+            connection.release();
             if (err) {
-              res.status(500).send({code: 500, msg: 'Database Error'});
+              res.status(500).send({ 
+                'code': 500, 
+                'msg': 'Database Error', 
+                'err': JSON.stringify(err)
+              });
             } else {
-              res.status(200).send({'result': 'OK'});
+              res.status(200).send({ 'result': 'OK' });
             }
           }
         );
       });
     }
     else {
-      res.status(500).send({code: 500, msg: 'Incorrect parameter'});
+      res.status(500).send({ code: 500, msg: 'Incorrect parameter' });
     }
   });
 
